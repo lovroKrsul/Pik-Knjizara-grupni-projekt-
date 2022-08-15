e159e7b2dc5fe03ee7565e517964206ea07ec1d7
Pukazec <skvorc.leo@gmail.com>
2022-08-15 10:43:28 +0200

    Worker start

---
+++ PIK_Knjizara/Controllers/WorkerDashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PIK_Knjizara.Controllers
+{
+    public class WorkerDashboardController : Controller
+    {
+        // GET: WorkerDashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
