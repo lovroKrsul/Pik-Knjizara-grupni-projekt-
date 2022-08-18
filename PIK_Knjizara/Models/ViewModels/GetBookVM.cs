baef762f20234da32869a59b8969e0c49fe9054f
Pukazec <skvorc.leo@gmail.com>
2022-08-18 11:04:17 +0200

    Buy book pt2

--- PIK_Knjizara/Models/ViewModels/GetBookVM.cs
+++ PIK_Knjizara/Models/ViewModels/GetBookVM.cs
@@ -1,6 +1,7 @@
 ﻿using PIK_Library.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,8 +10,10 @@ namespace PIK_Knjizara.Models.ViewModels
     public class GetBookVM
     {
         public Book Book { get; set; }
+        [Display (Name = "Payment method:")]
         public bool PaymentMethod { get; set; }
         public bool Delivery { get; set; }
+        [Display (Name = "Return Date")]
         public DateTime ReturnDate { get; set; }
     }
 }
